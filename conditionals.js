2147ff32ae827150ae58fc30f2870c4f2de05895
Michael Macaulay <mike.macaulay@gmail.com>
2022-09-07 14:43:03 +0000

    Change conditional logic to check voting age

--- conditionals.js
+++ conditionals.js
@@ -1,16 +1,6 @@
-const raining = true;
-const temperature = 12;
+const isCitizen = true;
+const age = 26;
 
-if (raining) {
-  console.log("Don't forget your umbrella!");
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
 }
-
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
-}
-
-console.log("Now you're ready to go outside!");
